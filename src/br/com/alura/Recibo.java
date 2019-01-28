7f4343fd18c378721fc3a0ccdbe983b2587ea594
gx2.marcio <marcio.mendonca@agibank.com.br>
2019-01-28 13:28:58 -0200

    Leitura de conjunto via Iterator

---
+++ src/br/com/alura/Recibo.java
@@ -0,0 +1,12 @@
+package br.com.alura;
+
+public class Recibo {
+
+	Recibo rec1 = new Recibo();
+	// atribui valores
+	Recibo rec2 = new Recibo();
+	// atribui valores
+	Recibo rec3 = new Recibo();
+	// atribui valores
+	
+}
--- src/br/com/alura/TestaCursoComAluno.java
+++ src/br/com/alura/TestaCursoComAluno.java
@@ -1,41 +1,55 @@
 package br.com.alura;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class TestaCursoComAluno {
 
 	public static void main(String[] args) {
 		Curso javaColecoes = new Curso("Dominando as colecoes do java", "Marcio Mendonca");
-		
+
 		javaColecoes.adiciona(new Aula("Trabalhando com ArrayLista", 21));
 		javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
 		javaColecoes.adiciona(new Aula("Modelando as colecoes", 22));
-		
+
 		Aluno a1 = new Aluno("Rodrigo Turini", 34672);
 		Aluno a2 = new Aluno("Guilherme Silveira", 5617);
 		Aluno a3 = new Aluno("Mauricio Aniche", 17645);
-		
+
 		javaColecoes.matricula(a1);
 		javaColecoes.matricula(a2);
 		javaColecoes.matricula(a3);
+
+		System.out.println("Todos os alunos matriculados nesse curso: ");
+
+//		javaColecoes.getAlunos().forEach(a -> {
+//			System.out.println(a);
+//		});
+
+		Set<Aluno> alunos = javaColecoes.getAlunos();
+		Iterator<Aluno> iterador = alunos.iterator();
+		while (iterador.hasNext()) {
+			Aluno proximo = iterador.next();
+			System.out.println(proximo);
+		}
 		
-		System.out.println("Todos os alunos matriculados nesse curso: " );
-		
-		javaColecoes.getAlunos().forEach(a -> {
-			System.out.println(a);
-		});
-		
-		System.out.println("O aluno "+ a1 + "esta matriculado");
+//		for (Aluno a : javaColecoes.getAlunos()) {
+//			System.out.println(a);
+//		}
+
+		System.out.println("O aluno " + a1 + "esta matriculado");
 		System.out.println(javaColecoes.estaMatriculado(a1));
-		
+
 		Aluno turini = new Aluno("Rodrigo Turini", 34672);
 		System.out.println("E esse Turini, esta matriculado?");
 		System.out.println(javaColecoes.estaMatriculado(turini));
-		
+
 		System.out.println("O a1 é == ao Turini?");
 		System.out.println(a1.equals(turini));
-		
+
 		// Obriatoriamente o seguinte é true:
-		
+
 		System.out.println(a1.hashCode() == turini.hashCode());
 	}
-	
+
 }
